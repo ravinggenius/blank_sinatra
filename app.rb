d65d4fe5e2709005fb71d450c1294fcff1d8be28
Raving Genius <rg+code@ravinggenius.com>
2010-02-10 22:06:44 -0500

    Reorganized app according to http://github.com/chriseppstein/compass-sinatra

--- app.rb
+++ app.rb
@@ -1,23 +1,26 @@
 require 'rubygems'
+require 'compass'
 require 'sinatra'
 require 'haml'
-require 'compass'
 
 configure do
+  set :app_file, __FILE__
+  set :root, File.dirname(__FILE__)
+  set :haml, { :format => :html5 }
+
   Compass.configuration do |config|
     config.project_path = File.dirname(__FILE__)
     config.sass_dir = 'views/stylesheets'
   end
 
-  set :haml, { :format => :html5 }
   set :sass, Compass.sass_engine_options
+end
+
+get '/styles/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :"stylesheets/#{params[:name]}"
 end
 
 get '/' do
   haml :index
-end
-
-get '/styles/screen.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  sass :'stylesheets/screen'
 end
