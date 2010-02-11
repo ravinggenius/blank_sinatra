e36e018bf6b4aac669346bc833dec2283b18153b
Raving Genius <rg+code@ravinggenius.com>
2010-02-10 21:41:46 -0500

    Added Compass and a stylesheet

--- app.rb
+++ app.rb
@@ -1,7 +1,23 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'compass'
+
+configure do
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = 'views/stylesheets'
+  end
+
+  set :haml, { :format => :html5 }
+  set :sass, Compass.sass_engine_options
+end
 
 get '/' do
   haml :index
+end
+
+get '/styles/screen.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :screen
 end
