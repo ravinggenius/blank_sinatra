398fda57c09536e5e39b50b8d6df40a8d975f58a
Raving Genius <rg+code@ravinggenius.com>
2010-06-21 23:34:24 -0400

    Added routes to Sinatra-derived class

--- routes.rb
+++ routes.rb
@@ -1,16 +1,18 @@
-get '/styles/:name.css' do
-  content_type :css, :charset => 'utf-8'
-  sass :"stylesheets/#{params[:name]}"
-end
+class Application < Sinatra::Base
+  get '/styles/:name.css' do
+    content_type :css, :charset => 'utf-8'
+    sass :"stylesheets/#{params[:name]}"
+  end
 
-get '/favicon.ico' do
-end
+  get '/favicon.ico' do
+  end
 
-get '/' do
-  haml :index
-end
+  get '/' do
+    haml :index
+  end
 
-get '/:slug' do
-  #not_found unless %w'clients contact services thanks'.include? params[:slug]
-  haml :"#{params[:slug]}"
+  get '/:slug' do
+    #not_found unless %w'clients contact services thanks'.include? params[:slug]
+    haml :"#{params[:slug]}"
+  end
 end
