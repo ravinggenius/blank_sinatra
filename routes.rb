572bda059bd7a022ae03e6b793d173be2a4625f0
Raving Genius <rg+code@ravinggenius.com>
2010-07-15 18:46:29 -0400

    [bugfix] Fixed issue introduced in commit 398fda5

--- routes.rb
+++ routes.rb
@@ -1,18 +1,16 @@
-class Application < Sinatra::Base
-  get '/styles/:name.css' do
-    content_type :css, :charset => 'utf-8'
-    sass :"stylesheets/#{params[:name]}"
-  end
+get '/styles/:name.css' do
+  content_type :css, :charset => 'utf-8'
+  sass :"stylesheets/#{params[:name]}"
+end
 
-  get '/favicon.ico' do
-  end
+get '/favicon.ico' do
+end
 
-  get '/' do
-    haml :index
-  end
+get '/' do
+  haml :index
+end
 
-  get '/:slug' do
-    #not_found unless %w'clients contact services thanks'.include? params[:slug]
-    haml :"#{params[:slug]}"
-  end
+get '/:slug' do
+  #not_found unless %w'clients contact services thanks'.include? params[:slug]
+  haml :"#{params[:slug]}"
 end

ravinggenius/blank_sinatra

commit 398fda57c09536e5e39b50b8d6df40a8d975f58a

Added routes to Sinatra-derived class

routes.rb
@@ -1,16 +1,18 @@
-get '/styles/:name.css' do
-  content_type :css, :charset => 'utf-8'
-  sass :"stylesheets/#{params[:name]}"
-end
+class Application < Sinatra::Base
+  get '/styles/:name.css' do
+    content_type :css, :charset => 'utf-8'
+    sass :"stylesheets/#{params[:name]}"
+  end
 
-get '/favicon.ico' do
-end
+  get '/favicon.ico' do
+  end
 
-get '/' do
-  haml :index
-end
+  get '/' do
+    haml :index
+  end
 
-get '/:slug' do
-  #not_found unless %w'clients contact services thanks'.include? params[:slug]
-  haml :"#{params[:slug]}"
+  get '/:slug' do
+    #not_found unless %w'clients contact services thanks'.include? params[:slug]
+    haml :"#{params[:slug]}"
+  end
 end

commit 572bda059bd7a022ae03e6b793d173be2a4625f0

[bugfix] Fixed issue introduced in commit 398fda5

routes.rb
@@ -1,18 +1,16 @@
-class Application < Sinatra::Base
-  get '/styles/:name.css' do
-    content_type :css, :charset => 'utf-8'
-    sass :"stylesheets/#{params[:name]}"
-  end
+get '/styles/:name.css' do
+  content_type :css, :charset => 'utf-8'
+  sass :"stylesheets/#{params[:name]}"
+end
 
-  get '/favicon.ico' do
-  end
+get '/favicon.ico' do
+end
 
-  get '/' do
-    haml :index
-  end
+get '/' do
+  haml :index
+end
 
-  get '/:slug' do
-    #not_found unless %w'clients contact services thanks'.include? params[:slug]
-    haml :"#{params[:slug]}"
-  end
+get '/:slug' do
+  #not_found unless %w'clients contact services thanks'.include? params[:slug]
+  haml :"#{params[:slug]}"
 end